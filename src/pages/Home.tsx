fdcfd8aa284df3cedcb196ba4d63cd4ff422c6ba
Wyatt Jebef <wyattjebef@Wyatts-MacBook-Pro.local>
2025-10-29 22:17:37 -0700

    Latest build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,8 @@
-import BoxLink from '../components/BoxLink';
 import styles from './projects.module.css';
 import PageSkeleton from './PageSkeleton';
 import PortraitBounce from '../components/PortraitBounce';
 
-type Project = {
-  link: string;
-  img: string;
-  pixel: boolean;
-  title: string;
-  description: string;
-}
-
 export default function Home() {
-  const projects: Project[] = [
-    { link: '/projects/crater', img: '/images/crater-tile.png', pixel: true, title: 'Crater', description: 'Digital Music Gram' },
-    { link: '/projects/lone-palm-island', img: '/images/lone-palm-island.png', pixel: false, title: 'Lone Palm Island', description: 'A 3D Demo Written in C++' },
-    { link: '/projects/postcard-2025', img: '/images/dive-into-2025-front.png', pixel: false, title: 'Dive Into 2025', description: 'Double Exposure Postcard' }
-  ];
-
   return (
     <PageSkeleton>
       <div className={styles['static-container']}>
@@ -39,13 +24,3 @@ export default function Home() {
     </PageSkeleton>
   );
 }
-
-
-{/* <div className={styles['box-links']}>
-          {projects.length > 0 && projects.map((p: Project) =>
-            <>
-              <BoxLink link={p.link} img={p.img} pixel={p.pixel} title={p.title} description={p.description} />
-              <br />
-            </>
-          )}
-        </div> */}
